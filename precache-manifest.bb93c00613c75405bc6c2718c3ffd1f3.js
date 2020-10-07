e0a6aab358a891899e12a7529bff8fe6dabe489e
SardulDhyani <sarduldhyani07@gmail.com>
2020-10-07 19:15:46 +0530

    Updates

---
+++ precache-manifest.bb93c00613c75405bc6c2718c3ffd1f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8903201d0f5ee77997a9d16ea10d5873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00797cda1bdd2f2783b2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c8f45d94630c1c966eda",
+    "url": "/static/js/2.9fb5d14f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fb5d14f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00797cda1bdd2f2783b2",
+    "url": "/static/js/main.6e8f9a1e.chunk.js"
+  },
+  {
+    "revision": "7edf34b6e00e5abed137",
+    "url": "/static/js/runtime-main.8276650f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
